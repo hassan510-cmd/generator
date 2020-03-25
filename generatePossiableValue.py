f46de808ad089c557735b9a7171a1a1d15c7dc82
hassan510-cmd <55447090+hassan510-cmd@users.noreply.github.com>
2020-03-25 02:15:24 +0200

    file

--- generatePossiableValue.py
+++ generatePossiableValue.py
@@ -1,34 +1,41 @@
+import csv
 from tabulate import tabulate
 import itertools
 from itertools import combinations_with_replacement
+import datetime
 # print(bsss)
 # print(arr)
 # _________________________________________
 
-print("""
-                    $$$$$$$$$$$$$$$$$   EEEEEEEEEEEEEEE   DDDDDDDDDDDDDDDDDD
-                    $$$$$$$$$$$$$$$$$   EEEEEEEEEEEEEEE   DDDDDDDDDDDDDDDDDDD             OOOOOOO
-                    $$$$$$$             EEEEE             DDDD           DDDDD          OOO     OOO
-                    $$$$$$$             EEEEE             DDDD            DDDDD        OOO       OOOO
-                    $$$$$$$$$$$$$$$$$   EEEEEEEEE         DDDD             DDDDD      OOOO       OOOO
-                    $$$$$$$$$$$$$$$$$   EEEEEEEEE         DDDD             DDDD       OOOO       OOOO
-                              $$$$$$$   EEEEE             DDDD            DDDD        OOOO       OOOO
-                              $$$$$$$   EEEEE             DDDD           DDDD           OOO     OOO
-                    $$$$$$$$$$$$$$$$$   EEEEEEEEEEEEEEE   DDDDDDDDDDDDDDDDDD              OOOOOOO
-                    $$$$$$$$$$$$$$$$$   EEEEEEEEEEEEEEE   DDDDDDDDDDDDDDDDD
 
-                                        $$$$$$$$$$$$$$$$$                               NNNNNN          NNN         AAAAAAAAAAAAAAAA
-                                        $$$$$$$$$$$$$$$$$                               NNN NNN         NNN         AAAAAAAAAAAAAAAA
-                                        $$$$$$$                      OOOOOOO            NNN  NNN        NNN         AAA          AAA
-                                        $$$$$$$                    OOO     OOO          NNN   NNN       NNN         AAA          AAA
-                                        $$$$$$$$$$$$$$$$$         OOO       OOOO        NNN    NNN      NNN         AAAAAAAAAAAAAAAA
-                                        $$$$$$$$$$$$$$$$$        OOOO       OOOO        NNN     NNN     NNN         AAAAAAAAAAAAAAAA
-                                                  $$$$$$$        OOOO       OOOO        NNN      NNN    NNN         AAA          AAA
-                                                  $$$$$$$        OOOO       OOOO        NNN       NNN   NNN         AAA          AAA
-                                        $$$$$$$$$$$$$$$$$          OOO     OOO          NNN        NNN  NNN         AAA          AAA
-                                        $$$$$$$$$$$$$$$$$            OOOOOOO            NNN         NNN NNN         AAA          AAA
-""")
-print()
+def sedosona():
+    print("""
+                        $$$$$$$$$$$$$$$$$   EEEEEEEEEEEEEEE   DDDDDDDDDDDDDDDDDD
+                        $$$$$$$$$$$$$$$$$   EEEEEEEEEEEEEEE   DDDDDDDDDDDDDDDDDDD             OOOOOOO
+                        $$$$$$$             EEEEE             DDDD           DDDDD          OOO     OOO
+                        $$$$$$$             EEEEE             DDDD            DDDDD        OOO       OOOO
+                        $$$$$$$$$$$$$$$$$   EEEEEEEEE         DDDD             DDDDD      OOOO       OOOO
+                        $$$$$$$$$$$$$$$$$   EEEEEEEEE         DDDD             DDDD       OOOO       OOOO
+                                $$$$$$$   EEEEE             DDDD            DDDD        OOOO       OOOO
+                                $$$$$$$   EEEEE             DDDD           DDDD           OOO     OOO
+                        $$$$$$$$$$$$$$$$$   EEEEEEEEEEEEEEE   DDDDDDDDDDDDDDDDDD              OOOOOOO
+                        $$$$$$$$$$$$$$$$$   EEEEEEEEEEEEEEE   DDDDDDDDDDDDDDDDD
+
+                                            $$$$$$$$$$$$$$$$$                               NNNNNN          NNN         AAAAAAAAAAAAAAAA
+                                            $$$$$$$$$$$$$$$$$                               NNN NNN         NNN         AAAAAAAAAAAAAAAA
+                                            $$$$$$$                      OOOOOOO            NNN  NNN        NNN         AAA          AAA
+                                            $$$$$$$                    OOO     OOO          NNN   NNN       NNN         AAA          AAA
+                                            $$$$$$$$$$$$$$$$$         OOO       OOOO        NNN    NNN      NNN         AAAAAAAAAAAAAAAA
+                                            $$$$$$$$$$$$$$$$$        OOOO       OOOO        NNN     NNN     NNN         AAAAAAAAAAAAAAAA
+                                                    $$$$$$$        OOOO       OOOO        NNN      NNN    NNN         AAA          AAA
+                                                    $$$$$$$        OOOO       OOOO        NNN       NNN   NNN         AAA          AAA
+                                            $$$$$$$$$$$$$$$$$          OOO     OOO          NNN        NNN  NNN         AAA          AAA
+                                            $$$$$$$$$$$$$$$$$            OOOOOOO            NNN         NNN NNN         AAA          AAA
+    """)
+    print()
+
+
+fName = str(datetime.datetime.now()).replace(":", "-").replace(".", "-")
 
 
 def generateFormChar():
@@ -36,13 +43,28 @@ def generateFormChar():
     arr = list(b.strip())
     x = len(arr)
     count = 0
+    print("THIS FOR : "+b)
     for i in range(1, x+1):
         l = list(itertools.product(arr, repeat=i))
         print("level" + str(i),
               " / number of possiable values in this level : " + str(len(l)))
         count += len(l)
+        with open(fName+'.csv', mode='a+', newline='') as filename:
+            filename = csv.writer(filename, delimiter='|',
+                                  quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            # filename.writerow(
+            #     "------------------------------------------------------------------")
+            # filename.writerow("THIS FOR : "+b)
+            filename.writerow(["level " + str(
+                i) + "/ number of possiable values in this level : "+str(len(l))])
+            # filename.writerow(
+            #     "_________________________")
+            filename.writerows(l)
+            # filename.writerow(
+            #     "------------------------------------------------------------------ End level"+str(i))
+
         print(tabulate(l, tablefmt="fancy_grid"))
-        print("------------------------------------------------------------------")
+    print("------------------------------------------------------------------")
     print("number of all values : ", str(count))
     print()
     cho = input(
@@ -58,7 +80,6 @@ def generateFormChar():
         print("thank you")
 
 
-# generateFormChar()
 def generateFormWord():
     b = input(
         "Enter your words separeted by ',' note that your white space will be included : ")
@@ -66,13 +87,22 @@ def generateFormWord():
     # print(arr)
     x = len(arr)
     count = 0
+    print("THIS FOR : "+b)
     for i in range(1, x+1):
         l = list(itertools.product(arr, repeat=i))
         print("level" + str(i),
               " / number of possiable values in this level : " + str(len(l)))
         count += len(l)
+        with open(fName+'.csv', mode='a+', newline='') as filename:
+            filename = csv.writer(filename, delimiter='|',
+                                  quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            # filename.writerow("THIS FOR : "+b)
+
+            filename.writerow(["level " + str(
+                i) + "/ number of possiable values in this level : "+str(len(l))])
+            filename.writerows(l)
         print(tabulate(l, tablefmt="fancy_grid"))
-        print("------------------------------------------------------------------")
+    print("------------------------------------------------------------------")
     print("number of all values : ", str(count))
     print()
     print(
@@ -101,4 +131,5 @@ def chooses():
         print("wrong choise")
 
 
+sedosona()
 chooses()
